612e997c0e8bb9fe7ca7aff0e6d7fef05ec00fec
Barnabas Nyansimera <75734424+barryombaki@users.noreply.github.com>
2022-01-25 07:51:03 -0500

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,7 @@
+#python script to enable password authentication
+ import getpass
+ #Check the password
+if passwd == "barry123":
+    print("You are authenticated")
+else:
+    print("You are not authenticated")
